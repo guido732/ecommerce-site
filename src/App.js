b5318b7f5c29bef42b76dcebd2296cdbebf59bdc
guido732 <guido732@gmail.com>
2020-02-09 22:41:29 -0300

    Adds sign-in component with custom input + button

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route path="/signin" component={SignInUp} />
 			</Switch>
 		</div>
 	);
