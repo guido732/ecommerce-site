4155912f1e68b2b7830fea8ab373cf5c05a8a235
Guido Torres <guido732@gmail.com>
2020-02-16 17:33:16 -0300

    creates cart dropdown component with styles and positioning

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import { auth } from "../../firebase/firebase.utils";
 
@@ -28,7 +30,9 @@ const Header = ({ currentUser }) => (
 					SIGN IN
 				</Link>
 			)}
+			<CartIcon />
 		</div>
+		<CartDropdown />
 	</div>
 );
 
