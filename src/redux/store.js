1cd12eceacfd84aa782dcd56d7015f26ef6d6c4b
guido732 <guido732@gmail.com>
2020-02-16 22:32:37 -0300

    Implements reselect for redux and implements selectors on cart selectors for shopping cart counter and dropdown items

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,10 @@ const middlewares = [logger];
 
 const store = createStore(
 	rootReducer,
-	compose(applyMiddleware(...middlewares), window.devToolsExtension ? window.devToolsExtension() : f => f)
+	compose(
+		applyMiddleware(...middlewares),
+		window.__REDUX_DEVTOOLS_EXTENSION__ ? window.window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+	)
 );
 
 export default store;
