5d3b9a2e565b8953e13e7669430164710b8728b5
Guido Torres <guido732@gmail.com>
2020-06-08 21:24:22 -0300

    fix (styled components - header) implements styled component 'as' to avoid over complicating definitions

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 import { auth } from "../../firebase/firebase.utils";
 
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from "./header.styles";
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
 	<HeaderContainer>
@@ -20,7 +20,9 @@ const Header = ({ currentUser, hidden }) => (
 			<OptionLink to="./shop">SHOP</OptionLink>
 			<OptionLink to="./contact">CONTACT</OptionLink>
 			{currentUser ? (
-				<OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+				<OptionLink as="div" onClick={() => auth.signOut()}>
+					SIGN OUT
+				</OptionLink>
 			) : (
 				<OptionLink to="./signin">SIGN IN</OptionLink>
 			)}
