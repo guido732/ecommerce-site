85580fa1cf8956ccebee72a1d0c2a5c630a7ac71
Guido Torres <guido732@gmail.com>
2020-06-16 17:49:00 -0300

    chore (Shop component) Refactors class component into functional component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,22 @@
 // React
-import React from "react";
+import React, { useEffect } from "react";
 // Router
 import { Route } from "react-router-dom";
 // Components
 import CollectionsOverviewContainer from "components/collections-overview/collections-overview.container";
 import CollectionsPageContainer from "pages/collection/collection.container";
 
-class ShopPage extends React.Component {
-	componentDidMount() {
-		const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+	useEffect(() => {
 		fetchCollectionsStart();
-	}
-	render() {
-		const { match } = this.props;
-		return (
-			<div className="shop-page">
-				<Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-				<Route path={`${match.path}/:collectionId`} component={CollectionsPageContainer} />
-			</div>
-		);
-	}
-}
+	}, [fetchCollectionsStart]);
+
+	return (
+		<div className="shop-page">
+			<Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+			<Route path={`${match.path}/:collectionId`} component={CollectionsPageContainer} />
+		</div>
+	);
+};
 
 export default ShopPage;
